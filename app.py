6b1a9a0fd601516d1713fe75d1f078cae4c5421f
Corin Faife <corin.faife@gmail.com>
2019-04-26 13:06:42 -0400

    Change app.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+import requests
+from bs4 import BeautifulSoup
+
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
@@ -7,4 +10,55 @@ def home():
     return generate_html()
 
 def generate_html():
-    return 'Hello, World!'
+    base_url = 'https://www.instructables.com'
+    homepage = requests.get(base_url)
+
+
+    # Create a BeautifulSoup object
+    soup = BeautifulSoup(homepage.text, 'html.parser')
+
+    instruction_categories = list()
+    # Find category links
+    for link in soup.find('div', {'id': "home-categories-menu"}):
+        instruction_categories.append(link.get('href'))
+
+
+    ## Search for individual instructions
+    test_topic = instruction_categories[0]
+    topic_page = requests.get(base_url + test_topic)
+    soup2 = BeautifulSoup(topic_page.text, 'html.parser')
+
+
+    article_list = list()
+
+    for link in soup2.findAll(class_='title'):
+        article_list.append(link.find('a').get('href'))
+        # Randomize selection here?
+
+    test_article = article_list[0]
+
+
+    # Get text from an individual article
+    instruction_page = requests.get(base_url+test_article)
+    instructable = BeautifulSoup(instruction_page.text, 'html.parser')
+
+
+    ##### 
+    # Work with the test page.
+    title = instructable.find('h1').text
+
+    step_titles = list()
+    for step in instructable.findAll(class_='step-title'):
+        step_titles.append(step.text)
+
+
+    step_body = list()
+    for text in instructable.findAll(class_='step-body'):
+        step_body.append(text.text)
+
+    
+    return str(step_titles)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
